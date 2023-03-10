bf7da0460e01d8d2da95c98d0ac663f92dd531e3
Eugene Kabeyi <eugenekabeyi144@gmail.com>
2023-03-10 05:17:05 +0300

    complied file dded

---
+++ fake.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+using namespace std;
+
+int main ()
+
+{
+    float weight;
+    weight=23.5;
+    return 0;
+}
--- new.cc
+++ new.cc
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
 
-    cout <<"Hello world";
+    cout <<"Hello world\n";
     int x;
     x=5;
     cout << x ;
